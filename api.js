9e1758ccaca0565ea52e1c43ba3f6c5638dd46da
tardyblaque0404 <cliffjonye@gmail.com>
2026-05-09 15:45:56 +0300

    fix api baseurl

--- api.js
+++ api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://workpay-uf12.onrender.com',
+  baseURL: 'https://workpay-uf12.onrender.com/api',
   headers: { 'Content-Type': 'application/json' },
 });
 
